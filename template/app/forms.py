d5c95bb7bfd7cd4eeb3e7b6a7205a2edde9605c8
Zoe <dadac123@gmail.com>
2014-06-09 23:38:35 +0800

    flask & rethinkdb

--- template/app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms.fields import TextField
-from wtforms.validators import Required
-
-class TaskForm(Form):
-	label = TextField('label', validators = [Required()])
